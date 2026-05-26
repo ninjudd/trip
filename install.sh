3e14f84e3ff881cc408f92675998a877494f0159
Justin Balthrop <git@justinbalthrop.com>
2026-05-25 22:08:55 -0700

    Add --dev flag to install.sh for symlink-based dev install

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 set -e
 
-echo "Building drip..."
-cargo build --release
-
-echo "Installing to /usr/local/bin/drip..."
-sudo cp target/release/drip /usr/local/bin/drip
+if [ "$1" = "--dev" ]; then
+    echo "Building drip (debug)..."
+    cargo build
+    echo "Linking to /usr/local/bin/drip..."
+    sudo ln -sf "$(pwd)/target/debug/drip" /usr/local/bin/drip
+else
+    echo "Building drip..."
+    cargo build --release
+    echo "Installing to /usr/local/bin/drip..."
+    sudo cp target/release/drip /usr/local/bin/drip
+fi
 
 echo "Done. Run 'drip enter' to get started."
